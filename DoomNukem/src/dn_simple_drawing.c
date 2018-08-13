f17768f72c0dabc64a92620df1a257678ffae69f
druzenko <33424074+druzenko@users.noreply.github.com>
2018-08-13 22:01:11 +0300

    fucking magic

---
+++ DoomNukem/src/dn_simple_drawing.c
@@ -0,0 +1,31 @@
+#include "dn_simple_drawing.h"
+#include "dn.h"
+
+void draw_vline( t_renderable *renderable, t_simple_vline line )
+{
+	int						*pixels;
+	int						y;
+
+	pixels = renderable->pixels;
+	line.y1 = CLAMP( line.y1, 0, renderable->h );
+	line.y2 = CLAMP( line.y2, 0, renderable->h );
+	if ( line.y2 < line.y1 )
+		return;
+	pixels[ ( line.y1 * renderable->w ) + line.x ] = 0xFF0000;
+	y = line.y1;
+	while ( y++ < line.y2 )
+		pixels[ ( y * renderable->w ) + line.x ] = line.color;
+	renderable->pixels[ line.y2 * renderable->w + line.x ] = 0xFF0000;
+}
+
+void draw_screen_simple( t_game_state *game_state )
+{
+	t_item	items[ 32 ];
+	t_item	*head = items;
+	t_item	*tail = items;
+
+	int *ytop = ft_memalloc( game_state->render_state.w );
+
+
+	free( ytop );
+}
--- DoomNukem/src/dn_simple_drawing.h
+++ DoomNukem/src/dn_simple_drawing.h
@@ -1,1 +1,19 @@
 #pragma once
+
+typedef struct	s_item
+{
+	int			sector_number;
+	int			sx1;
+	int			sx2;
+}				t_item;
+
+typedef struct	s_simple_vline
+{
+	int			x;
+	int			y1;
+	int			y2;
+	int			color;
+}				t_simple_vline;
+
+void	draw_vline( struct s_renderable *renderable, t_simple_vline line );
+void	draw_screen_simple( struct s_game_state *game_state );
